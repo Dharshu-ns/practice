5346e71211c88dbf72960050696804591ad2cbaf
dharshu_ns <dharshanans54@gmail.com>
2025-02-15 23:45:29 +0530

    array reverse using for loop

--- array.js
+++ array.js
@@ -1,3 +1,13 @@
 const arr = [1,2,3,4,5];
 const reversedArr = arr.reverse(); //buildin method
-console.log(reversedArr); 
+
+//Using for loop
+function reversedArray(arr){
+    const reversed = [];
+    for(let i=arr.length-1; i>=0;i--){
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+
+console.log(reversedArray([1,2,3,4,5,6]));
